730d9c8d4c56a5583d8647c95d8507ad5c52a178
LanHuong1507 <20057651.huong@student.iuh.edu.vn>
2023-11-25 10:23:19 +0700

    update list paging

--- src/main/java/vn/edu/iuh/fit/www_week05/backend/services/CandidateServices.java
+++ src/main/java/vn/edu/iuh/fit/www_week05/backend/services/CandidateServices.java
@@ -20,26 +20,7 @@ public class CandidateServices {
     public Page<Candidate> findAll(int pageNo, int pageSize, String sortBy, String sortDirection) {
         Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
         Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
-        return candidateRepository.findAll(pageable);//findFirst.../findTop...
+        return candidateRepository.findAll(pageable);
     }
 
-    public Page<Candidate> findPaginated(Pageable pageable) {
-        int pageSize = pageable.getPageSize();
-        int currentPage = pageable.getPageNumber();
-        int startItem = currentPage * pageSize;
-        List<Candidate> list;
-        List<Candidate> candidates = candidateRepository.findAll();
-
-        if (candidates.size() < startItem) {
-            list = Collections.emptyList();
-        } else {
-            int toIndex = Math.min(startItem + pageSize, candidates.size());
-            list = candidates.subList(startItem, toIndex);
-        }
-
-        Page<Candidate> candidatePage
-                = new PageImpl<>(list, PageRequest.of(currentPage, pageSize), candidates.size());
-
-        return candidatePage;
-    }
 }
--- src/main/java/vn/edu/iuh/fit/www_week05/fontend/Controllers/CandidateController.java
+++ src/main/java/vn/edu/iuh/fit/www_week05/fontend/Controllers/CandidateController.java
@@ -2,6 +2,7 @@ package vn.edu.iuh.fit.www_week05.fontend.Controllers;
 
 
 import com.neovisionaries.i18n.CountryCode;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -29,7 +30,7 @@ public class CandidateController {
     @Autowired
     private CandidateRepository candidateRepository;
     @Autowired
-    private CandidateServices candidateServices;
+    private CandidateServices candidateService;
     @Autowired
     private AddressRepository addressRepository;
     @Autowired
@@ -42,15 +43,13 @@ public class CandidateController {
     }
 
     @GetMapping("/list-paging")
-    public String showCandidateListPaging(Model model,
-                                          @RequestParam("page") Optional<Integer> page,
-                                          @RequestParam("size") Optional<Integer> size) {
+    public String showListCandidatePaging(Model model
+            , @RequestParam("page") Optional<Integer> page
+            , @RequestParam("size") Optional<Integer> size) {
+
         int currentPage = page.orElse(1);
         int pageSize = size.orElse(10);
-        /*Page<Candidate> candidatePage= candidateServices.findPaginated(
-                PageRequest.of(currentPage - 1, pageSize)
-        );*/
-        Page<Candidate> candidatePage = candidateServices.findAll(currentPage - 1,
+        Page<Candidate> candidatePage = candidateService.findAll(currentPage - 1,
                 pageSize, "id", "asc");
 
         model.addAttribute("candidatePage", candidatePage);
@@ -64,7 +63,6 @@ public class CandidateController {
         }
         return "candidates/list";
     }
-
     @GetMapping("/show-add-form")
     public ModelAndView add(Model model) {
         ModelAndView modelAndView = new ModelAndView();
@@ -92,7 +90,7 @@ public class CandidateController {
     public ModelAndView edit(@PathVariable("id") long id) {
         ModelAndView modelAndView = new ModelAndView();
         Optional<Candidate> opt = candidateRepository.findById(id);
-        if (opt.isPresent()) {
+        if(opt.isPresent()) {
             Candidate candidate = opt.get();
             modelAndView.addObject("candidate", candidate);
             modelAndView.addObject("address", candidate.getAddress());
@@ -101,17 +99,14 @@ public class CandidateController {
         }
         return modelAndView;
     }
-
     @PostMapping("/candidates/update/{id}")
     public String update(
             @ModelAttribute("candidate") Candidate candidate,
             @ModelAttribute("address") Address address) {
         addressRepository.save(address);
-//        candidate.setAddress(address);
         candidateRepository.save(candidate);
-        return "redirect:/candidates/update";
+        return "redirect:/list";
     }
-
     @GetMapping("/candidates/delete/{id}")
     public String delete(@PathVariable("id") long id) {
         Optional<Candidate> optionalCandidate = candidateRepository.findById(id);
